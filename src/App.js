7fb64518de296f6559bc7dd75f97ab173b43c61a
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-11-27 14:00:03 +0530

    create Accordion component ...

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import Button from "./Button";
+import Accordion from "./components/Accordion";
 
-import { GoBell } from 'react-icons/go';
-import { GoCloud } from 'react-icons/go';
-import { GoDatabase } from 'react-icons/go';
 
 function App() {
 
-  const handleClick = () => {
-    console.log('Clicked!!')
-  }
-
+  const items = [
+    {
+      id: "sbajs",
+      label: "Section 1",
+      content: "Section-1 content ..."
+    },
+    {
+      id: "sbsajsnajs",
+      label: "Section 2",
+      content: "Section-2 content ..."
+    },
+    {
+      id: "sbabsasnjs",
+      label: "Section 3",
+      content: "Section-3 content ..."
+    }
+  ];
+  
   return (
     <>
-      <div>
-        <Button secondary onClick={handleClick} classNames="mb-2"> 
-          <GoBell></GoBell>
-          abcd
-          </Button>
-      </div>
-      <div>
-        <Button success rounded outline onClick={handleClick}>
-          <GoCloud></GoCloud>
-          abcd
-          </Button>
-      </div>
-      <div>
-        <Button danger onClick={handleClick}>
-          <GoDatabase></GoDatabase>
-          abcd
-          </Button>
-      </div>
-      <div>
-        <Button warning onClick={handleClick}>abcd</Button>
-      </div>
-      <div>
-        <Button primary outline onClick={handleClick}>abcd</Button>
-      </div>
+      <Accordion items={items} />
     </>
   );
 }
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,43 @@
+import Button from '../components/Button';
+
+import { GoBell } from 'react-icons/go';
+import { GoCloud } from 'react-icons/go';
+import { GoDatabase } from 'react-icons/go';
+
+function ButtonPage() {
+
+  const handleClick = () => {
+    console.log('Clicked!!')
+  }
+
+  return (
+    <>
+      <div>
+        <Button secondary onClick={handleClick} classNames="mb-2"> 
+          <GoBell></GoBell>
+          abcd
+          </Button>
+      </div>
+      <div>
+        <Button success rounded outline onClick={handleClick}>
+          <GoCloud></GoCloud>
+          abcd
+          </Button>
+      </div>
+      <div>
+        <Button danger onClick={handleClick}>
+          <GoDatabase></GoDatabase>
+          abcd
+          </Button>
+      </div>
+      <div>
+        <Button warning onClick={handleClick}>abcd</Button>
+      </div>
+      <div>
+        <Button primary outline onClick={handleClick}>abcd</Button>
+      </div>
+    </>
+  );
+}
+
+export default ButtonPage;
